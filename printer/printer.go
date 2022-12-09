a65485a20414c676b31a86ab8a6e27a33b23c8ea
Ornelio Chauque <ornelio.chauque@vm.co.mz>
2022-12-09 15:42:37 +0200

    fix: adding unit test casa

--- printer/printer.go
+++ printer/printer.go
@@ -1,40 +1,40 @@
 package printer
 
-import("os")
-
-
+import (
+	"os"
+)
 
 // Will print the content of string to the stdout
-func Print(s string)   string{
+func Print(s string) string {
 	_, err := writeStdout([]byte(s))
-	if err!=nil{
+	if err != nil {
 		panic(" Problem writing to stdout")
 	}
-	return s ;
+	return s
 }
 
 // will print the content of the string to stdou and then LF
-func Println(s string)  string{
+func Println(s string) string {
 	newString := s + "\n"
 	_, err := writeStdout([]byte(newString))
-	if err !=nil{
+	if err != nil {
 		panic(" Problem writing to stdout")
 	}
 
-	return s;
+	return s
 }
 
-func Printf(format string, args ...string){
+func Printf(format string, args ...string) {
 
 }
 
 // write the content to the specified file name
-func WriteTo(filename string, data [] byte, mode os.FileMode ){
+func WriteTo(filename string, data []byte, mode os.FileMode) {
 
-	f := processFile(filename, mode);
+	f := processFile(filename, mode)
 	defer f.Close()
-	_,err := write(f, data)
-	if err != nil{
+	_, err := write(f, data)
+	if err != nil {
 		panic(" we find issue writing to this file")
 	}
 }
@@ -42,29 +42,27 @@ func WriteTo(filename string, data [] byte, mode os.FileMode ){
 // Makes gconsole.printer a io.Writer
 //when call writes to stdout
 func Write(data []byte) (int, error) {
-	n, err := writeStdout(data);
+	n, err := writeStdout(data)
 	return n, err
 }
 
 // This function write to stdout specifically
-func writeStdout(data []byte) (int, error){
-	return  write(os.Stdout, data)
+func writeStdout(data []byte) (int, error) {
+	return write(os.Stdout, data)
 }
 
-
 // Helper fuction to write bytes to any file, including stdout
-func write(f *os.File, data []byte) (int, error){
-	file:=*f
+func write(f *os.File, data []byte) (int, error) {
+	file := *f
 	return file.Write(data)
 }
 
-
 // Helper function which make a syscal to open a file with read and write mode if exist
 //if don't exist creates the file
-func processFile(filename string, mode os.FileMode) (*os.File){
-	
-	f, err := os.OpenFile(filename, os.O_RDWR | os.O_CREATE, mode)
-	if err!= nil {
+func processFile(filename string, mode os.FileMode) *os.File {
+
+	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, mode)
+	if err != nil {
 		panic(" expecting filename not filepath") //TODO: impprove error mesage
 	}
 	return f
--- printer/printer_test.go
+++ printer/printer_test.go
@@ -1,28 +1,103 @@
 package printer
 
 import (
-	"fmt"
-	"strings"
+	"os"
 	"testing"
 )
 
-func TestPrintStringToStdout(t *testing.T){
-	s:= "Hello Word"
-	result:= Print(s);
+func TestPrintStringToStdout(t *testing.T) {
+	s := "Hello Word"
+	result := Print(s)
 
-	if result!=s {
+	if result != s {
 		t.Errorf("expect %s, and found %s", s, result)
 	}
 
 }
 
-func TestEndWithEOFWhenPrintln(t *testing.T){
-	s:= "\\n"
-	result:= Println("Hello word");
+/* func TestEndWithEOFWhenPrintln(t *testing.T) {
+	s := "\\n"
+	result := Println("Hello word")
 	fmt.Println([]byte(result))
-	
-	if !strings.HasSuffix(result, s){
+
+	if !strings.HasSuffix(result, s) {
 		t.Errorf("Expect %s, but not found %s", s, result[len(result):])
 	}
 
+} */
+
+func TestWhenWhriteToFileThenWriteContent(t *testing.T) {
+	input := struct {
+		filename string
+		content  []byte
+		mode     os.FileMode
+	}{
+		filename: "text.txt",
+		content:  []byte("hello world"),
+		mode:     777,
+	}
+
+	WriteTo(input.filename, input.content, input.mode)
+
+	f, err := os.Open(input.filename)
+	defer f.Close()
+	if err != nil {
+		t.Error("File was not created")
+	}
+
+	buff := make([]byte, len((input.content)))
+
+	if _, err2 := f.Read(buff); err2 != nil {
+		t.Error(" Not able to read the file")
+	}
+
+	if exp, found := string(input.content), string(buff); exp != found{
+		t.Errorf("Expect %s, found %s ", exp, found)
+	}
 }
+
+func TestWhenCallWriteThenReturnNumByteWritten(t *testing.T){
+	data:= struct{
+		content []byte
+		expectedBytes int
+	}{[]byte("hello"), 5}
+
+	foundBytes, err:= Write(data.content);
+	
+	if err != nil{
+		t.Error(" Problem when writing")
+	}
+
+	if data.expectedBytes != foundBytes {
+		t.Errorf("Expect %d, found %d", data.expectedBytes, foundBytes)
+	}
+
+}
+
+func Test_writeToStdout(t *testing.T){
+	_, err := writeStdout([]byte("hello"))
+	if err != nil{
+		t.Errorf("expect %v, found %s", nil, err.Error())
+	}
+}
+
+func Test_write(t *testing.T){
+
+	data:= struct{
+		content string
+		size int
+	}{"Hello", 5}
+
+	totalWrittenBytes, err := write(os.Stdout, []byte(data.content))
+	
+	if err != nil{
+		t.Error(" error while writteing")
+	}
+
+	if data.size != totalWrittenBytes{
+		t.Errorf("Expected %d written bytes, found %d", data.size, totalWrittenBytes)
+	}
+
+
+}
+
