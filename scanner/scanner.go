979b8767eee2e4c3ff6762c73aaab959acef3a88
Ornélio Chaúque <chauque.ornelio@gmail.com>
2022-12-09 15:53:21 +0200

    Fix/unit test (#5)
    * fix: solving stash integration issue
    
    * chore: add .gitignore
    
    * fix: adding unit test casa
    
    Co-authored-by: Ornelio Chauque <ornelio.chauque@vm.co.mz>

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -3,14 +3,15 @@ package scanner
 import (
 	"os"
 )
+
 // Get the user input from console and retun as s string
-func Get() string{
-	s :=os.Stdin
+func Get() string {
+	s := os.Stdin
 	buff := make([]byte, 128)
-	_, err:= s.Read(buff)
+	_, err := s.Read(buff)
 	defer s.Close()
-	if err!=nil {
+	if err != nil {
 		panic(" something unexpected happens, can't read from stdio")
 	}
-	return string(buff);
+	return string(buff)
 }
---
+++ scanner/scanner_test.go
@@ -0,0 +1,11 @@
+package scanner
+
+import "testing"
+
+
+func TestHello (t *testing.T){
+
+	if "hello" != "hello"{
+		t.Error("error")
+	}
+}
