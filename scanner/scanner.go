e9e95f723137e08c163c297b2eddb1d8f5eb3a17
Ornelio Chauque <ornelio.chauque@vm.co.mz>
2022-12-13 15:04:30 +0200

    feat: add method to get byte representation of the string received from stdin

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -31,3 +31,30 @@ func Get() string {
 	
 	return inputResult.String()
 }
+
+//GetBytes return the  slice of byte representation of content read from Stdin
+func GetBytes() []byte {
+	s := os.Stdin
+	buff := make([]byte, BUFFER_SIZE)
+	defer s.Close()
+    var err error;
+	inputResult := make([]byte, BUFFER_SIZE)
+
+	for n:=0; err == nil;{
+		
+		n,err = s.Read(buff)
+		if err != nil {
+			panic(" something unexpected happens, can't read from stdio")
+		}
+	
+		inputResult = append(inputResult, buff...)
+		if n < BUFFER_SIZE{
+			break
+		}
+
+
+	}
+	
+	return inputResult
+}
+
