64e134863c1bc37c2657e43afdaa83ddf6d2e973
Ornelio Chauque <ornelio.chauque@vm.co.mz>
2022-12-13 13:47:41 +0200

    fix: allow scanner package to accept any amount of byte as input from stdin

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -2,16 +2,33 @@ package scanner
 
 import (
 	"os"
+	"strings"
 )
-
+const (
+	BUFFER_SIZE = 256// set a fixed buffer size, to allow better use of memory
+)
 // Get the user input from console and retun as s string
 func Get() string {
 	s := os.Stdin
-	buff := make([]byte, 128)
-	_, err := s.Read(buff)
+	buff := make([]byte, BUFFER_SIZE)
 	defer s.Close()
-	if err != nil {
-		panic(" something unexpected happens, can't read from stdio")
+    var err error;
+	var inputResult strings.Builder
+
+	for n:=0; err == nil;{
+		
+		n,err = s.Read(buff)
+		if err != nil {
+			panic(" something unexpected happens, can't read from stdio")
+			break
+		}
+		inputResult.Write(buff[:n])
+		if n < BUFFER_SIZE{
+			break
+		}
+
+
 	}
-	return string(buff)
+	
+	return inputResult.String()
 }
